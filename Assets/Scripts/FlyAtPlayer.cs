86f5e6109df09806f5ba0d23002f77feb9f65488
Keigo <kurzawakeigo@gmail.com>
2025-11-07 15:39:28 +0700

    second commit

--- Assets/Scripts/FlyAtPlayer.cs
+++ Assets/Scripts/FlyAtPlayer.cs
@@ -13,6 +13,18 @@ public class FlyAtPlayer : MonoBehaviour
 
     void Update()
     {
+        MoveToPlayer();
+        DestroyWhenReached();
+    }
+
+    void DestroyWhenReached()
+    {
+        if (transform.position == playerPosition)
+            Destroy(gameObject);
+    }
+
+    void MoveToPlayer()
+    {
         transform.position = Vector3.MoveTowards(transform.position, playerPosition, timeMoving * Time.deltaTime);
     }
 }
