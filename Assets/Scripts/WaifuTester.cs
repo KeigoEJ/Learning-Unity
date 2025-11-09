6001869ff7d9f729fade5291d0ecc54b0b73bcca
Keigo <kurzawakeigo@gmail.com>
2025-11-09 17:20:16 +0700

    Fourth commit

---
+++ Assets/Scripts/WaifuTester.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class WaifuTester : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Debug.Log("Belle soo cute!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
